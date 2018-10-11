e59131a0d7813f9ce5d3dd2be29a3acca1067f6a
Unknown <felix.sabatie@gmail.com>
2018-10-11 17:04:41 -0400

    Draw circle where finger is

--- app/src/main/java/com/example/felix/gestures_test/CustomCanvas.kt
+++ app/src/main/java/com/example/felix/gestures_test/CustomCanvas.kt
@@ -5,13 +5,29 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.view.View
 
-class CustomCanvas(context: Context) : View(context) {
+class CustomCanvas(context: Context): View(context) {
+
+    private var circleX = 0F
+    private var circleY = 0F
+    private val paint = Paint()
+    var drawCircle = false
+
+    init {
+        paint.style = Paint.Style.FILL
+        paint.setARGB(255, 255, 0, 0)
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        val paint = Paint()
-        paint.setStyle(Paint.Style.FILL)
-        paint.setARGB(255, 255, 0, 0)
-        canvas.drawCircle(50F, 50F, 50F, paint)
+        if(drawCircle)
+            canvas.drawCircle(circleX, circleY, 50F, paint)
     }
+
+    fun refreshCircle(x: Float, y: Float) {
+        circleX = x
+        circleY = y
+        this.invalidate()
+    }
+
 }
--- app/src/main/java/com/example/felix/gestures_test/MainActivity.kt
+++ app/src/main/java/com/example/felix/gestures_test/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.example.felix.gestures_test
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
+import android.view.View
 import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mainCanvas: CustomCanvas
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val mainLayout = findViewById<LinearLayout>(R.id.main_layout)
-        mainLayout.addView(CustomCanvas(this))
+        mainCanvas = CustomCanvas(this)
+        mainLayout.addView(mainCanvas)
+
+        mainLayout.setOnTouchListener() { view, motionEvent -> onLayoutClick(view, motionEvent) }
     }
 
+    private fun onLayoutClick(v: View, motionEvent: MotionEvent): Boolean {
+        if (motionEvent.action == MotionEvent.ACTION_DOWN)
+            mainCanvas.drawCircle = true
+        else if (motionEvent.action == MotionEvent.ACTION_UP)
+            mainCanvas.drawCircle = false
+
+        mainCanvas.refreshCircle(motionEvent.x, motionEvent.y)
+        return true
+    }
 
 }
